35ecf6206bf10fa3127985e8ec0976a3d1b726f2
professorDeveloper <azamovhud007@outlook.com>
2024-08-28 09:47:29 +0500

    Day2 Task4

--- task5/task4.cpp
+++ task5/task4.cpp
@@ -9,4 +9,11 @@ int main() {
     int fileSize = 128;
     int result = fileSize * 1000;
     cout << "File KB Size: " << result << endl;
+
+
+    //Task2
+    double fileSizeMb = 1.8  * 1000;
+    double netSpeedMb = 750 * 0.001;
+    double resultTime = fileSizeMb / netSpeedMb;
+    cout << "file download time: " << resultTime << endl;
 }

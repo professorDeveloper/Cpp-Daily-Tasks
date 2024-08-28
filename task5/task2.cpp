49b483f068b35b3fdafbae55c118cd1b07100e2c
professorDeveloper <azamovhud007@outlook.com>
2024-08-28 09:16:26 +0500

    Day2 Task2

---
+++ task5/task2.cpp
@@ -0,0 +1,24 @@
+//
+// Created by azamov on 8/28/2024.
+//
+
+#include <complex>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main() {
+    //Task1
+    int x = 2;
+    int y = x * x;
+    int z = y * y;
+    int w = z * z;
+    cout << w << endl;
+
+    //Task2
+    int hour = 24;
+    int day = 2;
+    int result = hour + (day * 24);
+    cout << result << endl;
+}

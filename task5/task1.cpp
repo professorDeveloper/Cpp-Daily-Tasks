1d0d27a731bc64408dcee4f253fd4a2427cd1d47
professorDeveloper <azamovhud007@outlook.com>
2024-08-28 09:09:40 +0500

    Day2 Task1

--- task5/task1.cpp
+++ task5/task1.cpp
@@ -1,3 +1,25 @@
 //
 // Created by azamov on 8/27/2024.
 //
+
+
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Task1
+    int x = 2;
+    int y = x * x;
+    cout << y * y << endl;
+
+    //Task2
+    int x2 = 2;
+    int y2 = x * x2;
+    int z = x2 * y2;
+    //For check
+    cout << pow(x2, 6) << endl;
+    cout << z*z << endl;
+
+    //Task3
+}

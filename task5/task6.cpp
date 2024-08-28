9a13a5806961650b0b3571658a36a06877155a00
professorDeveloper <azamovhud007@outlook.com>
2024-08-28 10:12:46 +0500

    Day2 Task5

--- task5/task6.cpp
+++ task5/task6.cpp
@@ -5,8 +5,21 @@
 using namespace std;
 
 int main() {
+    int totalPage = 750;
+    int rowOFPage = 50;
+    int charOfRow = 152;
+    int totalChars = rowOFPage * (charOfRow * totalPage);
+    int totalBookSizeMb = totalChars / 1000000;
 
+    cout << totalBookSizeMb << endl;
 
+    //Task2
+    int n = 20;
+    cout << "Enter n:";
+    cin >> n;
+    float word = 5;
+    float result =  2.5 * n;
+    cout << result / word << endl;
 
     return 0;
 }

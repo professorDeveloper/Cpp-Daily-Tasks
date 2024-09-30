c36db3e8d3460499bf4e8b5e7e27701224b759d8
professorDeveloper <azamovhud007@outlook.com>
2024-10-01 01:19:47 +0500

    2 module task 11-12 done

--- module2/task13/task2.cpp
+++ module2/task13/task2.cpp
@@ -3,10 +3,18 @@
 //
 #include <iostream>
 using namespace std;
-
 int main() {
-    int s =500;
-    int p =12;
-
-
+    cout<<"summa";
+    double s;cin>>s;
+    cout<<"Foizni kiriting: ";
+    double p;cin>>p;
+    double oy = 0;
+    double boshSum = 2 * s;
+    while (s < boshSum) {
+        s += s * (p / 100);
+        oy++;
+    }
+    cout<<oy<<" oydan so'ng boshlang'ich qiymatidan 2 marta ko'p "<<endl;
+    cout<<oy<< "- oydagi pul miqdori = "<<s;
 }
+
---
+++ module2/task13/task3.cpp
@@ -0,0 +1,27 @@
+//
+// Created by azamov on 10/1/2024.
+//
+#include <iostream>
+using namespace std;
+int main() {
+    int a ;
+    int b;
+    int c;
+    cout << "Enter a number : ";
+    cin >> a;
+    cout << "Enter a number : ";
+    cin >> b;
+    cout << "Enter a number : ";
+    cin >> c;
+    if (a < b) {
+        swap(a, b);
+    }
+
+    if (a<c) {
+        swap(a, c);
+    }
+    if (b < c) {
+        swap(b, c);
+    }
+    cout << a << " " << b << " "<< c << endl;
+}
